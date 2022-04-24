7eb34087fe5496a8c46d5a4e671ed9041faf0e97
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-04-24 19:50:33 +0300

    Загрузка чистого проекта

---
+++ app/src/main/java/com/arhi_app/tortam/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.arhi_app.tortam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
