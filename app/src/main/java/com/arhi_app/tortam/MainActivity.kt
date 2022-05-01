fc58d30cb17bddaab8868406c5a602b50cc3619d
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-05-01 13:37:44 +0300

    Вывел свои кнопки и настроил под них регистрацию и вход

--- app/src/main/java/com/arhi_app/tortam/MainActivity.kt
+++ app/src/main/java/com/arhi_app/tortam/MainActivity.kt
@@ -3,6 +3,7 @@ package com.arhi_app.tortam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
@@ -16,6 +17,11 @@ import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
     private lateinit var tvAccount:TextView
+    private lateinit var tvAccountLogin:TextView
+    private lateinit var tvAccountAnd:TextView
+    private lateinit var tvRegistr:TextView
+    private lateinit var tvActExit:TextView
+
     private lateinit var rootElement: ActivityMainBinding
     private val dialogHelper = DialogHelper(this)
     val mAuth = FirebaseAuth.getInstance()
@@ -40,7 +46,31 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
         rootElement.navView.setNavigationItemSelectedListener(this)
         tvAccount = rootElement.navView.getHeaderView(0).findViewById(R.id.tvAccountEmail)
+
+        tvAccountLogin = rootElement.navView.getHeaderView(0).findViewById(R.id.tvLogIn)
+        tvAccountAnd = rootElement.navView.getHeaderView(0).findViewById(R.id.tvAnd)
+        tvRegistr = rootElement.navView.getHeaderView(0).findViewById(R.id.tvRegistr)
+        tvActExit = rootElement.navView.getHeaderView(0).findViewById(R.id.tvActExit)
+
+        tvAccountLogin.setOnClickListener {
+            dialogHelper.createSignDialog(DialogConst.SIGN_IN_STATE)
+            hideAccountTextAuthMenu()
+            rootElement.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        tvRegistr.setOnClickListener {
+            dialogHelper.createSignDialog(DialogConst.SIGN_UP_STATE)
+            hideAccountTextAuthMenu()
+            rootElement.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+
+        tvActExit.setOnClickListener {
+            uiUpdate(null)
+            mAuth.signOut()
+            rootElement.drawerLayout.closeDrawer(GravityCompat.START)
+        }
     }
+
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
@@ -71,11 +101,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         return true
     }
 
+    private fun hideAccountTextAuthMenu(){
+        tvAccountLogin.visibility = View.GONE
+        tvAccountAnd.visibility = View.GONE
+        tvRegistr.visibility = View.GONE
+
+        tvAccount.visibility = View.VISIBLE
+        tvActExit.visibility = View.VISIBLE
+    }
+    private fun showAccountTextAuthMenu(){
+        tvAccountLogin.visibility = View.VISIBLE
+        tvAccountAnd.visibility = View.VISIBLE
+        tvRegistr.visibility = View.VISIBLE
+
+        tvAccount.visibility = View.GONE
+        tvActExit.visibility = View.GONE
+    }
+
     fun uiUpdate(user:FirebaseUser?){
         tvAccount.text = if (user == null){
             resources.getString(R.string.not_reg)
         }else{
             user.email
+        }
+
+        if (user == null){
+            showAccountTextAuthMenu()
         }
     }
 }
