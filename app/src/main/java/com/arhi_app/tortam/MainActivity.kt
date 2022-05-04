1b5341f141c7208b72714e554c387ed0c474ed85
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-05-04 22:29:33 +0300

    2.5 поиск ошибок при регистрации или входе

--- app/src/main/java/com/arhi_app/tortam/MainActivity.kt
+++ app/src/main/java/com/arhi_app/tortam/MainActivity.kt
@@ -160,6 +160,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             showTextFieldsHeader()
         } else {
             tvAccountEmail.text = user.email
+            hideTextFieldsHeader()
         }
     }
 }
--- app/src/main/java/com/arhi_app/tortam/accounthelper/AccountHelper.kt
+++ app/src/main/java/com/arhi_app/tortam/accounthelper/AccountHelper.kt
@@ -1,13 +1,15 @@
 package com.arhi_app.tortam.accounthelper
 
+import android.util.Log
+import android.view.View
 import android.widget.Toast
 import com.arhi_app.tortam.MainActivity
 import com.arhi_app.tortam.R
+import com.arhi_app.tortam.constants.FirebaseAuthConstants
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.*
 import com.google.firebase.ktx.Firebase
 
 class AccountHelper(act: MainActivity) {
@@ -27,6 +29,27 @@ class AccountHelper(act: MainActivity) {
                             act.resources.getString(R.string.sign_up_error),
                             Toast.LENGTH_LONG
                         ).show()
+                        Log.d("MyLog", "Exception : ${task.exception}")
+                        if (task.exception is FirebaseAuthUserCollisionException){
+                            val exception = task.exception as FirebaseAuthUserCollisionException
+
+                            if (exception.errorCode == FirebaseAuthConstants.ERROR_EMAIL_ALREADY_IN_USE){
+//                                Log.d("MyLog", "Exception : ${exception.errorCode}")
+                                Toast.makeText(act, FirebaseAuthConstants.ERROR_EMAIL_ALREADY_IN_USE, Toast.LENGTH_LONG).show()
+                            }
+                        }else if (task.exception is FirebaseAuthInvalidCredentialsException){
+                            val exception = task.exception as FirebaseAuthInvalidCredentialsException
+                            if (exception.errorCode == FirebaseAuthConstants.ERROR_INVALID_EMAIL){
+                                Toast.makeText(act, FirebaseAuthConstants.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                        if (task.exception is FirebaseAuthWeakPasswordException){
+                            val exception = task.exception as FirebaseAuthWeakPasswordException
+                            Log.d("MyLog", "Exception : ${exception.errorCode}")
+                            if (exception.errorCode == FirebaseAuthConstants.ERROR_WEAK_PASSWORD){
+                                Toast.makeText(act, FirebaseAuthConstants.ERROR_WEAK_PASSWORD, Toast.LENGTH_LONG).show()
+                            }
+                        }
                     }
                 }
         }
@@ -39,11 +62,16 @@ class AccountHelper(act: MainActivity) {
                     act.uiUpdate(task.result?.user)
                     act.hideTextFieldsHeader()
                 } else {
-                    Toast.makeText(
-                        act,
-                        act.resources.getString(R.string.sign_in_error),
-                        Toast.LENGTH_LONG
-                    ).show()
+//                    Log.d("MyLog", "Exception : ${task.exception}")
+                    if (task.exception is FirebaseAuthInvalidCredentialsException){
+                        val exception = task.exception as FirebaseAuthInvalidCredentialsException
+                        Log.d("MyLog", "Exception : ${exception.errorCode}")
+                        if (exception.errorCode == FirebaseAuthConstants.ERROR_INVALID_EMAIL){
+                            Toast.makeText(act, FirebaseAuthConstants.ERROR_INVALID_EMAIL, Toast.LENGTH_LONG).show()
+                        }else if(exception.errorCode == FirebaseAuthConstants.ERROR_WRONG_PASSWORD){
+                            Toast.makeText(act, FirebaseAuthConstants.ERROR_WRONG_PASSWORD, Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
             }
         }
@@ -54,7 +82,7 @@ class AccountHelper(act: MainActivity) {
 //настройки сообщения
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
             // custom_default_web_client_id скоприровал из "D:\AppTortam\app\build\generated\res\google-services\debug\values\values.xml" - почемуто не работало
-            .requestIdToken(act.getString(R.string.custom_default_web_client_id)).build()
+            .requestIdToken(act.getString(R.string.custom_default_web_client_id)).requestEmail().build()
         return GoogleSignIn.getClient(act, gso)
     }
     //ПОЛУЧАЕМ КЛИЕНТА
@@ -72,6 +100,9 @@ class AccountHelper(act: MainActivity) {
         act.mAuth.signInWithCredential(credential).addOnCompleteListener { task->
             if (task.isSuccessful){
                 Toast.makeText(act, "Sign in done", Toast.LENGTH_LONG).show()
+                act.uiUpdate(task.result?.user)
+            }else{
+                Log.d("MyLog", "Google Sign In Exception : ${task.exception}")
             }
         }
     }
