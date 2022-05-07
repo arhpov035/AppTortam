9b29845767fe4c1f60276c385f2375fc83a37b75
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-05-07 22:26:41 +0300

    3.1 Считывание данных из json и добавление в activity

--- app/src/main/java/com/arhi_app/tortam/act/EditAdsAct.kt
+++ app/src/main/java/com/arhi_app/tortam/act/EditAdsAct.kt
@@ -2,8 +2,10 @@ package com.arhi_app.tortam.act
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.arhi_app.tortam.R
 import com.arhi_app.tortam.databinding.ActivityEditAdsBinding
+import com.arhi_app.tortam.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
     private lateinit var rootElement: ActivityEditAdsBinding
@@ -11,5 +13,15 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         rootElement = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(rootElement.root)
+        // создаём адаптер и подключаем к нашему спинеру
+        // в него передаём контекс, разметку, массив
+        val adapter = ArrayAdapter(
+            this,
+            android.R.layout.simple_spinner_item,
+            CityHelper.getAllCountries(this)
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // берем наш спинер
+        rootElement.spContry.adapter = adapter
     }
 }
