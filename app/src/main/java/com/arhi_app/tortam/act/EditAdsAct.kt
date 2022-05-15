1290ac19ac35b2e9e422a46ddcae67481abbbc13
arhpov035 <78172721+arhpov035@users.noreply.github.com>
2022-05-15 16:35:50 +0300

    Рабочая библиотека pix image

--- app/src/main/java/com/arhi_app/tortam/act/EditAdsAct.kt
+++ app/src/main/java/com/arhi_app/tortam/act/EditAdsAct.kt
@@ -1,20 +1,25 @@
 package com.arhi_app.tortam.act
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.arhi_app.tortam.R
 import com.arhi_app.tortam.databinding.ActivityEditAdsBinding
 import com.arhi_app.tortam.dialogs.DialogSpinnerHelper
 import com.arhi_app.tortam.utils.CityHelper
+import com.arhi_app.tortam.utils.ImagePicker
+import com.fxn.pix.Pix
+import com.fxn.utility.PermUtil
+
 
 class EditAdsAct : AppCompatActivity() {
     lateinit var rootElement: ActivityEditAdsBinding
-
     // создаём инстанцию класса DialogSpinnerHelper
     private val dialog = DialogSpinnerHelper()
+    private var isImagesPermissionGranted = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +27,31 @@ class EditAdsAct : AppCompatActivity() {
         setContentView(rootElement.root)
         init()
 
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PermUtil.REQUEST_CODE_ASK_MULTIPLE_PERMISSIONS -> {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isImagesPermissionGranted = true
+                } else {
+                    isImagesPermissionGranted = false
+                    Toast.makeText(
+                        this,
+                        "Approve permissions to open Pix ImagePicker",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                return
+            }
+        }
     }
 
     private fun init() {
@@ -51,5 +81,9 @@ class EditAdsAct : AppCompatActivity() {
         }else{
             Toast.makeText(this, "No country selected", Toast.LENGTH_LONG).show()
         }
+    }
+
+    fun onClickGetImages(view: View) {
+        ImagePicker.getImages(this)
     }
 }
